0e47092df262be62842f49a103d6b9e00e63f118
benilovj <benilov@gmail.com>
2011-06-26 19:41:00 +0200

    added the implementation

--- lib/nanoc-tilt.rb
+++ lib/nanoc-tilt.rb
@@ -1,7 +1,28 @@
 require "nanoc-tilt/version"
+require 'tempfile'
 
-module Nanoc
-  module Tilt
-    # Your code goes here...
+module Nanoc3::Filters
+  class TiltFilter < Nanoc3::Filter
+    identifier :tilt
+    type :text
+
+    def run(content, params={})
+      require 'tilt'
+
+      # Create context
+      context = ::Nanoc3::Context.new(assigns)
+
+      # Get result
+      proc = content ? lambda { content } : nil
+      Tilt.new(path_to_tiltable_file_for(content)).render(context, assigns, &proc)
+    end
+
+    protected
+    def path_to_tiltable_file_for(content)
+      tempfile = Tempfile.new(["tilt", "." + @item[:extension]])
+      tempfile << content
+      tempfile.close
+      tempfile.path
+    end
   end
 end
